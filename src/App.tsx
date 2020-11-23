11685b8f19d35251f9c20861836c087a9f85ce85
Eryck Soares <rcksrs@outlook.com>
2020-11-23 18:06:19 -0300

    Estrutura inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import HomePage from 'pages/HomePage';
+
+export default function App() {
+  return (
+    <HomePage />
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import 'antd/dist/antd.css';
+import 'assets/styles/bootstrap-grid.min.css';
+import 'assets/styles/styles.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
